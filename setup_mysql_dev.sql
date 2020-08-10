024d07502505cee7fc889b49ec263ba3b2938780
OctopusHugz <colsonscott53@gmail.com>
2020-08-10 06:16:17 +0000

    Added SQL comments to top of scripts

--- setup_mysql_dev.sql
+++ setup_mysql_dev.sql
@@ -1,3 +1,5 @@
+-- This script creates the hbnb_dev_db database and hbnb_dev user
+-- This script won't fail if the database or user already exists
 CREATE DATABASE IF NOT EXISTS hbnb_dev_db;
 CREATE USER IF NOT EXISTS 'hbnb_dev'@'localhost' IDENTIFIED BY 'hbnb_dev_pwd';
 GRANT ALL PRIVILEGES ON hbnb_dev_db TO 'hbnb_dev'@'localhost';
