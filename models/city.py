42d49a0ec8e8bf757341ef7ebc9e7906fa1d8e6d
OctopusHugz <colsonscott53@gmail.com>
2020-08-11 04:58:10 +0000

    Updated classes for SQLAlchemy

--- models/city.py
+++ models/city.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 """ City Module for HBNB project """
-from sqlalchemy import ForeignKey, Column, String
+from sqlalchemy import ForeignKey, Column, Relationship, String
 from models.base_model import Base, BaseModel
 
 
@@ -9,3 +9,4 @@ class City(BaseModel, Base):
     __tablename__ = 'cities'
     state_id = Column(String(60), ForeignKey('states.id'), nullable=False)
     name = Column(String(128), nullable=False)
+    places = Relationship("Place", cascade="all, delete", backref="cities")
--- models/user.py
+++ models/user.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 """This module defines a class User"""
 from models.base_model import Base, BaseModel
-from sqlalchemy import Column, String
+from sqlalchemy import Column, Relationship, String
 
 
 class User(BaseModel, Base):
@@ -11,3 +11,5 @@ class User(BaseModel, Base):
     password = Column(String(128), nullable=False)
     first_name = Column(String(128), nullable=True)
     last_name = Column(String(128), nullable=True)
+    places = Relationship("Place", cascade="all, delete", backref="user")
+    reviews = Relationship("Review", cascade="all, delete", backref="user")
