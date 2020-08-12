5bdd9d0a9ff9db07dfae9aa7eb30bdc7e59c1770
jicruz96 <accounts@jicruz.com>
2020-08-12 23:29:07 +0000

    remove test_console.py tests as an experiment

--- tests/test_console.py
+++ tests/test_console.py
@@ -8,10 +8,11 @@ from io import StringIO
 
 class TestHBNBCommandClass(TestCase):
     """ Tests HBNBCommand class """
+    pass
 
-    def test_do_create(self):
-        pass
-        # """ Tests all method """
-        # with patch('sys.stdout', new=StringIO()) as f:
-        #     HBNBCommand().onecmd("create bad")
-        #     self.assertEqual(f.getvalue(), "** class doesn't exist **\n")
+    # def test_do_create(self):
+    #     pass
+    #     """ Tests all method """
+    #     with patch('sys.stdout', new=StringIO()) as f:
+    #         HBNBCommand().onecmd("create bad")
+    #         self.assertEqual(f.getvalue(), "** class doesn't exist **\n")
