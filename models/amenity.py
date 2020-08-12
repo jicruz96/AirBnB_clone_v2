562aa483c78f434aadd1a96648694ef6788a4280
jicruz96 <accounts@jicruz.com>
2020-08-12 03:51:57 +0000

    Add tablename to amenity class

--- models/amenity.py
+++ models/amenity.py
@@ -3,11 +3,10 @@
 from sqlalchemy.orm import relationship
 from sqlalchemy.sql.schema import Column
 from sqlalchemy.sql.sqltypes import String
-from models.base_model import BaseModel
+from models.base_model import BaseModel, Base
 
 
-class Amenity(BaseModel):
-    pass
-    # __tablename__ = 'amenities'
-    # name = Column(String(128), nullable=False)
-    # place_amenities = relationship("")
+class Amenity(BaseModel, Base):
+    """ This class defines Amenity """
+    __tablename__ = 'amenities'
+    name = Column(String(128), nullable=False)
