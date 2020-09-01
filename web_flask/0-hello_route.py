88cdffc01b59f8136d0bd94a412660566a0007e7
jicruz96 <accounts@jicruz.com>
2020-09-01 22:22:17 +0000

    re-commit file 0

--- web_flask/0-hello_route.py
+++ web_flask/0-hello_route.py
@@ -1,8 +1,7 @@
 #!/usr/bin/python3
 """ starts a Flask web app """
 
-from flask import Flask, render_template
-
+from flask import Flask
 app = Flask(__name__)
 
 
@@ -10,48 +9,3 @@ app = Flask(__name__)
 def home():
     """ returns page for home page """
     return "Hello HBNB!"
-
-
-@app.route('/hbnb', strict_slashes=False)
-def hbnb():
-    """ returns page for /hbnb page """
-    return "HBNB"
-
-
-@app.route('/c/<text>', strict_slashes=False)
-def c(text):
-    """ returns page for c page """
-    return "C {}".format(text.replace("_", " "))
-
-
-@app.route('/python', defaults={'text': "is cool"})
-@app.route('/python/<text>', strict_slashes=False)
-def python(text):
-    """ returns page for python page """
-    return "Python {}".format(text.replace("_", " "), text)
-
-
-@app.route('/number/<int:n>', strict_slashes=False)
-def number(n):
-    """ returns page for number page if input is a number """
-    return "{} is a number".format(n)
-
-
-@app.route('/number_template/<int:n>', strict_slashes=False)
-def number_template(n):
-    """ returns page for number_template page if input is a number """
-    return render_template('5-number.html', n=n)
-
-
-@app.route('/number_odd_or_even/<int:n>', strict_slashes=False)
-def number_bro(n):
-    """ returns page for number_odd_or_even page if input is a number """
-    return render_template('6-number_odd_or_even.html', n=n)
-
-
-if __name__ == "__main__":
-    app.run(
-        host='0.0.0.0',
-        port=5000,
-        debug=True
-    )
