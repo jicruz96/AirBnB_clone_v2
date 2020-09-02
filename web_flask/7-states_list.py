224a12ef52bb6803750e011b5de3931bf4df90d9
jicruz96 <accounts@jicruz.com>
2020-09-02 20:18:27 +0000

    make state_list sorting occur in html template (7-states_list.html) rather than in the python file (7-states_list.py)

--- web_flask/7-states_list.py
+++ web_flask/7-states_list.py
@@ -18,14 +18,11 @@ def remove_session(dummy):
 def states():
     """ returns states page """
     states = list(storage.all("State").values())
-    states = [(state.id, state.name) for state in states]
-    states.sort()
     return render_template('7-states_list.html', states=states)
 
 
 if __name__ == "__main__":
 
-    # Your web application must be listening on 0.0.0.0, port 5000
     app.run(
         host='0.0.0.0',
         port=5000,
