fa66df08ed95ce1de2506150c186962d1e90af88
jicruz96 <accounts@jicruz.com>
2020-09-02 16:28:02 +0000

    change getter for cities attribute in State class

--- models/state.py
+++ models/state.py
@@ -3,16 +3,32 @@
 from sqlalchemy.orm import relationship
 from models.base_model import Base, BaseModel, Column, String
 from models.city import City
+from os import environ
 
 
 class State(BaseModel, Base):
     """ State class """
+
     __tablename__ = 'states'
     name = Column(String(128), nullable=False)
     cities = relationship("City", cascade="all, delete-orphan",
                           backref="state", passive_deletes=True)
 
-    @property
-    def cities(self):
-        """ Cities getter """
-        return self.cities
+    if environ.get('HBNB_TYPE_STORAGE') == 'db':
+
+        @property
+        def cities(self):
+            """ Cities getter """
+            return self.cities
+    else:
+        @property
+        def cities(self):
+            """ Cities getter """
+            from models import storage
+            from models.city import City
+            my_cities = []
+            cities = storage.all(City)
+            for city in cities.values():
+                if city.state_id == self.id:
+                    my_cities.append(city)
+            return my_cities
